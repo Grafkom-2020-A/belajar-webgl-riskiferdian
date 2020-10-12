e678cff180c7354abe89015fc2f4c5e3ba30958c
riskiferdian <riskiferdian9@gmail.com>
2020-10-12 09:33:32 +0700

    progres blm selesai

--- main.js
+++ main.js
@@ -2,21 +2,28 @@ function main() {
   var canvas = document.getElementById("myCanvas");
   var gl = canvas.getContext("webgl");
 
-  var vertexShaderCode = `
-  void main() {
-      
-  }
-  `;
+  // Definisi verteks-verteks pada segitiga
+  /**
+   * A (-0.5, 0.5); B (-0.5, -0.5); C (0.5, -0.5)
+   */
+  var vertices = [
+    -0.5, 0.5,      // Titik A 
+    -0.5, -0.5,     // Titik B
+    0.5, -0.5       // Titik C
+  ];
+
+  var positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  var vertexShaderCode = document.getElementById("vertexShaderCode").text;
 
   var vertexShader = gl.createShader(gl.VERTEX_SHADER);
   gl.shaderSource(vertexShader, vertexShaderCode);
   gl.compileShader(vertexShader);
 
-  var fragmentShaderCode = `
-  void main() {
-
-  }
-  `;
+  var fragmentShaderCode = document.getElementById("fragmentShaderCode").text;
 
   var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
   gl.shaderSource(fragmentShader, fragmentShaderCode);
@@ -28,6 +35,16 @@ function main() {
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
 
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(aPosition);
+
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
+
+  var primitive = gl.LINE_LOOP;
+  var offset = 0;
+  var count = 3;  // Jumlah verteks yang akan digambar
+  gl.drawArrays(primitive, offset, count);
 }
